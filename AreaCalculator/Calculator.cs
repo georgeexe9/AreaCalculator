c3a7761798bd6b7a48e8da835948101d89bb88f3
Georgi Lavchanski <149933409+georgeexe9@users.noreply.github.com>
2025-05-22 15:48:02 +0300

    Add project files.

---
+++ AreaCalculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    internal class Calculator
+    {
+       public double SquareArea(double x)
+        {
+            return x * x;
+        }
+        public double TriangleArea(double BaseLength, double height)
+        {
+            return 0.5 * BaseLength * height;
+        }
+        public double CircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        public double RectangleArea(double side, double height)
+        {
+            return side * height;
+        }
+        public double EclipseArea(double b, double a)
+        {
+            return Math.PI * a * b;
+        }
+        public double TrapezoidArea(double sideA, double sideB, double height)
+        {
+            return 0.5 * (sideA + sideB) * height;
+        }
+    }
+}
---
+++ AreaCalculator/Form1.cs
@@ -0,0 +1,10 @@
+namespace AreaCalculator
+{
+    public partial class CalculatorMainFrom : Form
+    {
+        public CalculatorMainFrom()
+        {
+            InitializeComponent();
+        }
+    }
+}
